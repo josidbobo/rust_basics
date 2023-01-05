6c5cdaa008debd63b565406e7532f926e8f8346b
Joshua Okoro <josidbobo@gmail.com>
2023-01-05 02:01:38 +0100

    update: Vector

--- src/vector.rs
+++ src/vector.rs
@@ -1,22 +1,20 @@
 #![allow(unused)]
 
-pub fn v_ector_and_others() -> String {
+use::std::fmt::Display;
+
+pub fn v_ector_and_others() {
 
     let vec1: Vec<i32> = Vec::new();
-    let mut vec2 : Vec<i32> = vec![1,2,3,4];
-    vec2.push(5);
-    println!("1st {}", vec2[0]);
+    let vec2 = vec![1,2,3,4];
 
     let second = &vec2[1];
     match vec2.get(1){
-        Some(second) =>  second.to_string(),
-        None => String::from("No 2nd value"),
-        _ => String::from("Last option"),
+        Some(second) =>  println!("There is a second {}", second),
+        None => println!("No 2nd value"),
     }
 
-
-
-
-
-
+    for mut i in vec2{
+        i *= 2;
+    }
 }
+
