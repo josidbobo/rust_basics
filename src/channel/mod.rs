7c39a6bd3998b02b36c7ad46fed8d93834f20224
Joshua Okoro <josidbobo@gmail.com>
2023-03-15 14:02:23 +0100

    fix: Module and Imports

--- src/channel/mod.rs
+++ src/channel/mod.rs
@@ -1,18 +1,18 @@
-#![allow(unused)]
-use std::env;
-use std::io::Write;
-use std::sync::mpsc::channel;
-use std::{io, thread};
+mod channel {
+    #![allow(unused)]
+    use std::env;
+    use std::io::Write;
+    use std::sync::mpsc::channel;
+    use std::{io, thread};
 
-fn main() {
+    fn main(){
     let (tx, rx) = channel();
 
-    let sender = thread::spawn(move || {
-        let args : Vec<String> = env::args().collect();
+    let sender = std::thread::spawn(move || {
+        let args : Vec<String> = std::env::args().collect();
         
         for i in args.iter(){
-        tx.send(i.to_owned())
-            .expect("Unable to send on channel");}
+        tx.send(i.to_owned()).expect("Unable to send on channel");}
     });
 
     let receiver = thread::spawn(move || {
@@ -22,4 +22,5 @@ fn main() {
 
     sender.join().expect("The sender thread has panicked");
     receiver.join().expect("The receiver thread has panicked");
+}
 }
